82d1145d9062a847d2218da56de5b1023b53fd66
Axxar <67249459+axxar@users.noreply.github.com>
2021-12-28 14:56:52 -0500

    init basic site structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <Navbar />
+      <Hero />
+    </>
+
   );
 }
 
