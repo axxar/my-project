1ffb52cfff5a695b4ffbf1335e5827ea7f3945db
Axxar <67249459+axxar@users.noreply.github.com>
2022-01-08 00:52:38 -0500

    deploy view fixes

--- src/components/intro/Introduction.js
+++ src/components/intro/Introduction.js
@@ -2,22 +2,23 @@ import React from 'react'
 
 const Introduction = () => {
     return (
-        <div className='flex h-screen'>
+        <div className='flex h-screen text-white bg-black'>
             <div className='flex items-center justify-center'>
                 <div className='p-12 h-1/2 flex flex-col'>
                     <h1 className='text-2xl h-10'>Hello, I'm</h1>
                     <h1 className='text-6xl font-bold pt-2'>Akshar Patel</h1>
                     <p className='pt-10'>
-                        I design and develop services using various technologies.  
-                        I design and develop services using various technologies.  
-                        I design and develop services using various technologies.  
+                        I design and develop services using various technologies.
+                    </p>
+                    <p className='pt-3'>
+                        I am also a photographer by passion.
                     </p>
                 </div>
             </div>
             <div className='flex-1 flex items-center justify-center'>
                 <div className='p-12 h-1/2 flex flex-col'>
-                    <p className='pt-10'>Image Here</p>
-                    https://www.youtube.com/watch?v=hQjlM-8C4Ps
+                    <p className='pt-10'>Image Coming Here Soon</p>
+                    {/* https://www.youtube.com/watch?v=hQjlM-8C4Ps */}
                 </div>
             </div>
         </div>
