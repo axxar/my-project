1bb02db591333f8a117bc7f79d205f536298cd1f
Axxar <67249459+axxar@users.noreply.github.com>
2022-01-09 11:33:24 -0500

    Landing Image in background

--- src/components/intro/Introduction.js
+++ src/components/intro/Introduction.js
@@ -1,12 +1,16 @@
 import React from 'react'
-
+import LandingImage from '../images/LandingImage.jpg'
 const Introduction = () => {
     return (
-        <div className='flex h-screen text-white bg-black'>
+        <div className='flex h-screen text-white bg-black' style={{
+            backgroundSize: "cover",
+            backgroundAttachment: "fixed",
+            backgroundImage: `url(${LandingImage})`,
+          }}>
             <div className='flex items-center justify-center'>
                 <div className='p-12 h-1/2 flex flex-col'>
-                    <h1 className='text-2xl h-10'>Hello, I'm</h1>
-                    <h1 className='text-6xl font-bold pt-2'>Akshar Patel</h1>
+                    <h1 className='text-3xl h-10'>Hello, I'm</h1>
+                    <h1 className='text-7xl font-bold pt-2'>Akshar Patel</h1>
                     <p className='pt-10'>
                         Software Developer By Profession.
                     </p>
