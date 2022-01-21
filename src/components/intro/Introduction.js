5541bdbedf01207084c11723b1e6ff9eea546d17
Axxar <67249459+axxar@users.noreply.github.com>
2022-01-21 18:59:41 -0500

    Responsive edits

--- src/components/intro/Introduction.js
+++ src/components/intro/Introduction.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import LandingImage from '../images/LandingImage.jpg'
+
 const Introduction = () => {
     return (
         <div className='flex h-screen text-white bg-black' style={{
@@ -7,22 +8,25 @@ const Introduction = () => {
             backgroundAttachment: "fixed",
             backgroundImage: `url(${LandingImage})`,
           }}>
-            <div className='flex items-center justify-center'>
-                <div className='p-12 h-1/2 flex flex-col'>
-                    <h1 className='text-3xl h-10'>Hello, I'm</h1>
-                    <h1 className='text-7xl font-bold pt-2'>Akshar Patel</h1>
-                    <p className='pt-10'>
-                        Software Developer By Profession.
-                    </p>
-                    <p className='pt-3'>
-                        Photographer By Passion.
-                    </p>
-                    <p className='pt-3'>
-                        Gamer By Night.
-                    </p>
+            <div className='md:flex'>
+                <div class="md:shrink-0">
+                    <div className="flex h-4/6 ml-10 mt-10">
+                        <div className='m-auto'>
+                            <h1 className='text-3xl h-10'>Hello, I'm</h1>
+                            <h1 className='text-7xl font-bold pt-2'>Akshar Patel</h1>
+                            <p className='pt-10'>Software Developer By Profession.</p>
+                            <p className='pt-3'>Photographer By Passion.</p>
+                            <p className='pt-3'>Gamer By Night.</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex h-4/6 ml-10 ">
+                    
                 </div>
             </div>
+            
         </div>
+        
     )
 }
 
