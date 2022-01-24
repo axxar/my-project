05dad89935053c1c182b1d9c9549e1e252d8e2c2
Axxar <67249459+axxar@users.noreply.github.com>
2022-01-23 23:22:47 -0500

    form changes for netlify

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,18 +8,22 @@ const Contact = () => {
         <div class="md:flex">
           <div class="md:shrink-0 sm:p-0 pl-4 md:w-6/12">
             <h1 className="text-4xl leading-normal">Let's connect</h1>
-            <form className="pt-6 pb-8 mb-4 flex flex-col" method="POST" data-netlify="true" >
+            <form className="pt-6 pb-8 mb-4 flex flex-col"
+                data-netlify="true"
+                name="cotactMe"
+                method="post"
+                data-netlify-recaptcha="true" netlify-honeypot="hpfield" data-netlify="true">
               <div className="w-full pr-3">
                 <div className="md:mb-0">
                   <label
                     className="uppercase tracking-wider text-white text-xs font-bold mb-2"
-                    for="company"
+                    for="name"
                   >
                     Name*
                   </label>
                   <input
                     className="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3"
-                    id="company"
+                    id="name"
                     type="text"
                     placeholder="Akshar P."
                     required
@@ -60,16 +64,12 @@ const Contact = () => {
                   </div>
                 </div>
               </div>
-              <div className="mb-2 pr-3">
-                <div className="mb-6 md:mb-0 py-3 px-4 pr-8 rounded">
-                  <div data-netlify-recaptcha="true"></div>
-                </div>
-              </div>
+              <div data-netlify-recaptcha="true"></div>
               <div className="pr-3 md:flex mt-2">
                 <div className="md:w-6/12 mx-auto px-3">
-                  <button type="submit" className="md:w-full bg-gray-900 text-white font-bold py-2 px-4 border-b-4 hover:border-b-2 border-gray-500 hover:border-gray-100 rounded-full">
-                    Send
-                  </button>
+                  <input type="submit" 
+                  className="md:w-full bg-gray-900 text-white font-bold py-2 px-4 border-b-4 hover:border-b-2 border-gray-500 hover:border-gray-100 rounded-full"
+                  value="Send" />
                 </div>
               </div>
             </form>
